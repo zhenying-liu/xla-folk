637e8acf0ba1862378916128aeb0a740c519eb47
Jane Liu <janeliu@nvidia.com>
2024-04-10 19:39:35 +0000

    Support NamedSharding with memory_kind by adding host memory spaces.

--- xla/pjrt/gpu/se_gpu_pjrt_client.cc
+++ xla/pjrt/gpu/se_gpu_pjrt_client.cc
@@ -57,6 +57,7 @@ limitations under the License.
 #include "xla/pjrt/gpu/gpu_helpers.h"
 #include "xla/pjrt/gpu/gpu_topology.h"
 #include "xla/pjrt/gpu/gpu_topology.pb.h"
+#include "xla/pjrt/host_memory_spaces.h"
 #include "xla/pjrt/local_device_state.h"
 #include "xla/pjrt/pjrt_client.h"
 #include "xla/pjrt/pjrt_compiler.h"
@@ -504,6 +505,12 @@ StreamExecutorGpuClient::StreamExecutorGpuClient(
     tensorflow::down_cast<PjRtStreamExecutorDevice*>(device)->AttachMemorySpace(
         memory_space.get());
     owned_memory_spaces_.push_back(std::move(memory_space));
+    const size_t basePinnedId = devices.size();
+    auto pinned =
+        std::make_unique<PinnedHostMemorySpace>(basePinnedId, device);
+    tensorflow::down_cast<PjRtStreamExecutorDevice*>(device)->AttachMemorySpace(
+        pinned.get());
+    owned_memory_spaces_.push_back(std::move(pinned));
   }
   for (const std::unique_ptr<PjRtMemorySpace>& memory_space :
        owned_memory_spaces_) {
